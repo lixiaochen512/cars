e91227facdf0016612d9028fbd1d5251d2632264
lixiaochen512 <819208895@qq.com>
2019-06-01 19:26:39 +0800

    异步上传图片

--- src/store/salecarStore/index.js
+++ src/store/salecarStore/index.js
@@ -1,7 +1,7 @@
 export default {
     namespaced:true,
     state:{
-        step:1
+        step:2
     },
     mutations:{
         changeStep(state,payload){
--- src/views/sale/Step2.vue
+++ src/views/sale/Step2.vue
@@ -1,14 +1,46 @@
 <template>
     <div>
-        <input type="file" ref="fileCtrl">
-        <button @click="upload">上传</button>
+        <!-- 预览图 -->
+        <img :src="imgUrl" v-if="imgUrl">
+
+        <input type="file" ref="fileCtrl" @change="changeHandle" multiple>
+        <Button type="primary" @click="upload">点击上传</Button>
     </div>
 </template>
 
 <script>
     export default {
+        data(){
+            return {
+                imgUrl: ''
+            }
+        },
         methods:{
-            async
+            changeHandle(){
+                // 创建文件读取器
+                const fileReader = new FileReader();
+                //获取用户选择的文件，转换URL地址为base64编码
+                fileReader.readAsDataURL(this.$refs.fileCtrl.files[0])
+                console.log(fileReader)
+                // 转换完毕（此时还没上传）
+                fileReader.onload = (e)=>{
+                    this.imgUrl = e.target.result;  //将base64图片存储到本地渲染
+                }
+
+            },
+            upload(){
+                const form = new FormData()
+                form.append('file',this.$refs.fileCtrl.files[0])
+                // console.log(form)
+                const xhr = new XMLHttpRequest()
+                xhr.onreadystatechange = function() {
+                    if (xhr.readyState === 4) {
+                        // console.log(xhr)
+                    }
+                }
+                xhr.open('post','http://127.0.0.1:3000/upload',true)
+                xhr.send(form)                
+            }
         }
     }
 </script>
